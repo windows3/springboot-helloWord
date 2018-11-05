de38dfa1eaee04c9e076d03d8341f70db0ec5b7f
何须回头 <416400304@qq.com>
2018-11-05 15:01:51 +0800

    第二种启动方式  控制扫描路径

--- src/main/java/com/windows3/HelloWordApplication.java
+++ src/main/java/com/windows3/HelloWordApplication.java
@@ -2,7 +2,9 @@ package com.windows3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.windows3.controller")
 @SpringBootApplication
 public class HelloWordApplication {
 
