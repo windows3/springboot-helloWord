e27c67e0a50b961c2b08040b920790528371a150
何须回头 <416400304@qq.com>
2018-11-05 14:18:59 +0800

    Initial commit FirstSpringBoot

---
+++ src/main/java/com/windows3/controller/HelloWordController.java
@@ -0,0 +1,32 @@
+package com.windows3.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 3 on 2018/11/5.
+ */
+//创建容器  tomcat
+@EnableAutoConfiguration
+//所有接口全部返回json格式
+@RestController
+public class HelloWordController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "success";
+    }
+    @RequestMapping("/getMsg")
+    public Map<String,Object> getMsg(){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("errorCode","200");
+        map.put("errorMsg","failed");
+        return map;
+    }
+
+
+}
